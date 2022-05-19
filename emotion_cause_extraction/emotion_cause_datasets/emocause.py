c925c33d9c35d56de90dab1081e0c99dde188e8a
Hongyuan Yan <hy@Hongyuans-MacBook-Pro.local>
2022-05-19 15:18:36 +0100

    Add EmotionCause

---
+++ emotion_cause_extraction/emotion_cause_datasets/emocause.py
@@ -0,0 +1,85 @@
+import json
+import logging
+
+from torch.utils.data import Dataset
+from tqdm import tqdm
+from transformers import AutoTokenizer
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+class EmoCauseDataset(Dataset):
+    def __init__(
+        self,
+        data_split,
+    ):
+        self.data_split = data_split
+        self.inputs = self._create_inputs()
+
+    def _create_inputs(self):
+        logging.info("Creating inputs...")
+        # Load Data:
+        raw_data = None
+        if self.data_split == "train":
+            raw_data_path = "emocause-datasets/train.json"
+        elif self.data_split == "test":
+            raw_data_path = "emocause-datasets/test.json"
+        else:
+            logging.error(
+                "The DATA_SPLIT parameter of EmoCauseDataset constructor is invalid.")
+        with open(raw_data_path, "r") as stream:
+            raw_data = json.load(stream)
+        # Define Tokenizer:
+        tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+        # Create Inputs:
+        inputs = []
+        for data in tqdm(raw_data[5:]):
+            # Tokenize Inputs:
+            context = data["original_situation"]
+            cause = set([cause[1] for cause in data["annotation"]])
+            emotion = data["emotion"]
+            emotion_clause = "The emotion of the context is: " + \
+                emotion[2: (len(emotion) - 2)]
+            tokenized_input = tokenizer(emotion_clause,
+                                        context,
+                                        max_length=128,
+                                        truncation="only_second",
+                                        stride=50,
+                                        return_offsets_mapping=True)
+            # Extract Sequence Labels:
+            word_ids = tokenized_input.word_ids()
+            sequence_ids = tokenized_input.sequence_ids()
+            labels = []
+            assert len(word_ids) == len(sequence_ids)
+            for i in range(len(word_ids)):
+                sequence_id = sequence_ids[i]
+                word_id = word_ids[i]
+                if sequence_id is None:
+                    labels.append(-100)
+                elif sequence_id == 0:
+                    labels.append(0)
+                elif sequence_id == 1:
+                    if word_id in cause:
+                        labels.append(1)
+                    else:
+                        labels.append(0)
+                else:
+                    logging.error(f"Sequence ID {sequence_id} is invalid.")
+            input = {
+                "input_ids": tokenized_input["input_ids"],
+                "attention_mask": tokenized_input["attention_mask"],
+                "label": labels,
+            }
+            inputs.append(input)
+        logging.info("Completed...")
+        return inputs
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        return self.inputs[index]
---
+++ emotion_cause_extraction/roberta.py
@@ -0,0 +1,96 @@
+import json
+import logging
+import numpy as np
+import os
+
+from datasets import Dataset, load_metric
+from transformers import AutoModelForTokenClassification, AutoTokenizer, Trainer, TrainingArguments
+
+from emotion_cause_datasets.emocause import EmoCauseDataset
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+id2label = {
+    "0": "O",
+    "1": "I"
+}
+label2id = {v: k for k, v in id2label.items()}
+
+
+def compute_metrics(eval_predictions) -> dict:
+    logits, labels = eval_predictions
+    predictions = np.argmax(logits, axis=-1)
+    metric = load_metric("seqeval")
+    return metric.compute(predictions=[predictions], references=[labels])
+
+
+def finetune_roberta(
+    dataset_train: Dataset,
+    dataset_valid: Dataset,
+    dataset_test: Dataset,
+    batch_size: int,
+    learning_rate: float,
+    num_train_epoch: int,
+    output_dir: str,
+    warmup_ratio: float,
+    weight_decay: float,
+):
+
+    args = TrainingArguments(
+        evaluation_strategy="epoch",
+        fp16=True,
+        greater_is_better=True,
+        learning_rate=learning_rate,
+        load_best_model_at_end=True,
+        logging_strategy="epoch",
+        num_train_epochs=num_train_epoch,
+        output_dir=output_dir,
+        per_device_eval_batch_size=(batch_size * 2),
+        per_device_train_batch_size=batch_size,
+        save_strategy="epoch",
+        warmup_ratio=warmup_ratio,
+        weight_decay=weight_decay,
+    )
+
+    model = AutoModelForTokenClassification.from_pretrained("roberta-base",
+                                                            id2label=id2label,
+                                                            label2id=label2id)
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=dataset_train,
+        eval_dataset=dataset_valid,
+        tokenizer=AutoTokenizer.from_pretrained("roberta-base", use_fast=True),
+        compute_metrics=compute_metrics,
+    )
+    logging.info(f"Training RoBERTa model ...")
+    trainer.train()
+
+    logging.info(f"Evaluating ...")
+    val_results = trainer.evaluate()
+    with open(os.path.join(output_dir, "val-results.json"), "w") as stream:
+        json.dump(val_results, stream, indent=4)
+    logging.info(f"Evaluation results: {val_results}")
+
+    logging.info(f"Testing ...")
+    test_results = trainer.predict(dataset_test)
+    with open(os.path.join(output_dir, "test-results.json"), "w") as stream:
+        json.dump(test_results.metrics, stream, indent=4)
+    logging.info(f"Test results: {test_results.metrics}")
+
+
+finetune_roberta(
+    EmoCauseDataset("train"),
+    EmoCauseDataset("test"),
+    EmoCauseDataset("test"),
+    16,
+    1e-5,
+    5,
+    "EmoCause_outputs",
+    0.2,
+    0.01,
+)
